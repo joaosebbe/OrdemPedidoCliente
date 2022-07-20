1f0d5549e4ac36613f08753785822c1670022a5f
joaosebbe <joaovitor.sebbe@yahoo.com>
2022-07-20 00:44:02 -0300

    Criando repositório

---
+++ OrdemPedidoCliente/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace OrdemPedidoCliente {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Criando Repositório");
+        }
+    }
+}
